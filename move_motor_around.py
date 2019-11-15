9bc71334f674f4d9e0a38ff51eb1b23b07364d76
mjbaucas <mjbaucas@gmail.com>
2019-11-15 16:33:16 -0500

    added test file for moving motor around

---
+++ move_motor_around.py
@@ -0,0 +1,17 @@
+'''
+Used to test out control over the motor
+
+Authors:
+Nathan Klassens
+Owen Kidnie
+'''
+
+import motor
+
+if __name__=="__main__":
+	while True:
+		angle = input("Enter angle: ") 
+		print angle
+		setAngle(angle)
+		time.sleep(2)
+
