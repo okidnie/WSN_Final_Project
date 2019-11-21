fe81c7246e002a98261b0347760445fb7a456d3f
Nathan Klassen <nthnklssn@live.com>
2019-11-21 12:01:40 -0500

    Fixed indents

--- main.py
+++ main.py
@@ -30,126 +30,125 @@ iterations = 25 # Number of times the pi scans for the beacons
 num_beacons = len(beacons)
 
 def getRSSIandTX():
-	'''
-	Obtains the RSSI and TX values of the packet
-	:return: rssi, and tx_power
-	'''
+    '''
+    Obtains the RSSI and TX values of the packet
+    :return: rssi, and tx_power
+    '''
 
-	# Value containers
-	rssi = [[]]
-	tx_power = [[]]
+    # Value containers
+    rssi = [[]]
+    tx_power = [[]]
 
-	# Open BLE socket
-	try:
-		sock = bluez.hci_open_dev(0)
-		print "Start BLE communication"
-	except:
-		print "error accessing bluetooth device..."
-		sys.exit(1)
-
-
-	blescan.hci_le_set_scan_parameters(sock)
-	blescan.hci_enable_le_scan(sock)
-
-	# Loop to scan the beacons
-	for x in range(iterations):
-
-		returnedList = blescan.parse_events(sock, 10)
-		for beacon in returnedList:
-			details = beacon.split(',')
-
-			# If scanned beacon is among the listed, store its RSSI and TX Power
-			if details[1] in beacons:
-				rssi[beacons.index(details[1])].append(float(details[5]))
-				tx_power[beacons.index(details[1])].append(float(details[4]))
-
-	# Makeshift data filter. Try to play around with this part of the code to get the best quality of RSSI and Tx_Power values
-	for x in range(num_beacons):
-		#Mean Filter
-		rssi[x] = sum(rssi[x])/float(len(rssi[x]))
-		tx_power[x] = sum(tx_power[x])/float(len(tx_power[x]))
+    # Open BLE socket
+    try:
+        sock = bluez.hci_open_dev(0)
+        print "Start BLE communication"
+    except:
+        print "error accessing bluetooth device..."
+        sys.exit(1)
 
 
-	print ("node ", rssi[0]," : ",  tx_power[0])
-	return rssi, tx_power
+    blescan.hci_le_set_scan_parameters(sock)
+    blescan.hci_enable_le_scan(sock)
+
+    # Loop to scan the beacons
+    for x in range(iterations):
+
+        returnedList = blescan.parse_events(sock, 10)
+        for beacon in returnedList:
+            details = beacon.split(',')
+
+            # If scanned beacon is among the listed, store its RSSI and TX Power
+            if details[1] in beacons:
+                rssi[beacons.index(details[1])].append(float(details[5]))
+                tx_power[beacons.index(details[1])].append(float(details[4]))
+
+            # Makeshift data filter. Try to play around with this part of the code to get the best quality of RSSI and Tx_Power values
+            for x in range(num_beacons):
+                #Mean Filter
+                rssi[x] = sum(rssi[x])/float(len(rssi[x]))
+                tx_power[x] = sum(tx_power[x])/float(len(tx_power[x]))
+
+
+    return rssi, tx_power
 
 # Calculates the distance between two points of interest using RSSI and TX Power
 def pathloss(rssi, tx_power):
-	'''
-	Calculates the distance between two points of interest using RSSI and TX Power
+    '''
+    Calculates the distance between two points of interest using RSSI and TX Power
 
-	:param rssi: 		The RSSI of the packet
-	:param tx_power: 	The TX power of the packet
-	:return:			The distance from the packet
-	'''
+    :param rssi: 		The RSSI of the packet
+    :param tx_power: 	The TX power of the packet
+    :return:			The distance from the packet
+    '''
 
-	n = 2.0
-	distance = 10.0**((tx_power - rssi) / (10.0 * n))
+    n = 2.0
+    distance = 10.0**((tx_power - rssi) / (10.0 * n))
 
-	return distance
+    return distance
 
 # Calculate location of poit of interest based on its distance from 3 defined points
 def trilateration(d1, d2, d3, p, q, r):
-	x = (d1**2 - d2**2 + p**2) / (2.0 * p)
-	y = ((d1**2 - d2**2 + q**2 + r**2) / (2.0 * r)) - ((q / r) * x)
+    x = (d1**2 - d2**2 + p**2) / (2.0 * p)
+    y = ((d1**2 - d2**2 + q**2 + r**2) / (2.0 * r)) - ((q / r) * x)
 
-	receiver = []
-	receiver.append(x)
-	receiver.append(y)
-	return receiver
+    receiver = []
+    receiver.append(x)
+    receiver.append(y)
+    return receiver
 
 def search(rssi, angle):
-		direction = -1 #default left
-		dir_change = 0
+    direction = -1 #default left
+    dir_change = 0
 
         while(1):
-			print("searhing...")
-			angle = motor.move(angle, direction)
-			rssi_new_list, tx_power_left = getRSSIandTX()
-			rssi_new = rssi_new_list[0]
-			#angle = motor.move(angle, 1)
-			#angle = motor.move(angle, 1)
-			#rssi_right_list, tx_power_right = getRSSIandTX()
-			#rssi_right = rssi_right_list[0]
+            print("searhing...")
+            angle = motor.move(angle, direction)
+            rssi_new_list, tx_power_left = getRSSIandTX()
+            rssi_new = rssi_new_list[0]
+            #angle = motor.move(angle, 1)
+            #angle = motor.move(angle, 1)
+            #rssi_right_list, tx_power_right = getRSSIandTX()
+            #rssi_right = rssi_right_list[0]
 
-			#print "rssi left: ", rssi_left
-			#print "rssi right: ", rssi_right
-			#print "rssi: ", rssi
+            #print "rssi left: ", rssi_left
+            #print "rssi right: ", rssi_right
+            #print "rssi: ", rssi
 
-			if rssi > rssi_new:
-				print "Changeing Direction"
-				direction = direction * -1
-				dir_change++
-				angle = motor.move(angle, direction)
-				if dir_change >= 3:
-					return angle, rssi
-			else:
-				print("IM CONFUSED")
-	return angle, rssi
+    if rssi > rssi_new:
+        print "Changeing Direction"
+        direction = direction * -1
+        dir_change++
+        angle = motor.move(angle, direction)
+        if dir_change >= 3:
+        return angle, rssi
+    else:
+        print("IM CONFUSED")
+    return angle, rssi
 
 # Main
 if __name__=="__main__":
-	angle = 90			# Current angle of the motor
-	value = 0			# Current RSSI value
+    angle = 90			# Current angle of the motor
+    value = 0			# Current RSSI value
     old_rssi = 0		# The max RSSI value in a cycle
-	motor.setAngle(90)
-	while(1):
-		rssi_list, tx_power = getRSSIandTX()
-		rssi = rssi_list[0]
+    motor.setAngle(90)
+    while(1):
+    rssi_list, tx_power = getRSSIandTX()
+    rssi = rssi_list[0]
 
-		if (abs(rssi-old_rssi) > 3):
-			#find node again
-			print("Target has moved")
-			angle, rssi = search(rssi, angle)
-			print("------------")
-			print("TARGET FOUND, at a distance {} away".format(pathloss(rssi)))
-			print("------------")
+    if (abs(rssi-old_rssi) > 3):
+    #find node again
+    print("Target has moved")
+    angle, rssi = search(rssi, angle)
+    print("------------")
+    print("TARGET FOUND, at a distance {} away".format(pathloss(rssi)))
+    print("------------")
 
 
-		old_rssi = rssi
+    old_rssi = rssi
 
-	# Debug Receiver value
-	print "rssi: {}".format(rssi)
+    # Debug Receiver value
+    print "rssi: {}".format(rssi)
 
-	# Clean motor pwm and GPIO
-	motor.motorCleanup()
+    # Clean motor pwm and GPIO
+    motor.motorCleanup()
--- motor.py
+++ motor.py
@@ -19,91 +19,91 @@ pwm.start(0)
 
 
 def setAngle(angle):
-	'''
-	Sets the angle of the servo motor
-	:param angle: the desired angle to set the servo motor to
-	'''
+    '''
+    Sets the angle of the servo motor
+    :param angle: the desired angle to set the servo motor to
+    '''
 
-	duty = angle/18 + 2
-	GPIO.output(03, True)
-	pwm.ChangeDutyCycle(duty)
-	time.sleep(1)
-	GPIO.output(03, False)
-	pwm.ChangeDutyCycle(0)
+    duty = angle/18 + 2
+    GPIO.output(03, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(03, False)
+    pwm.ChangeDutyCycle(0)
 
 def move(angle, direction):
-	'''
-	Moves the servo motor left or right by 18 degrees
-	:param angle: 		The current angle of the servo motor
-	:param direction: 	The desired direction (left or right)
-	:return: 			The new angle of the servo motor
-	'''
+    '''
+    Moves the servo motor left or right by 18 degrees
+    :param angle: 		The current angle of the servo motor
+    :param direction: 	The desired direction (left or right)
+    :return: 			The new angle of the servo motor
+    '''
 
-	if direction == 1:
-		print "Turning right"
-		angle = angle + 18
-	elif direction == -1:
-		print "Turning left"
-		angle = angle - 18
+    if direction == 1:
+        print "Turning right"
+        angle = angle + 18
+    elif direction == -1:
+        print "Turning left"
+        angle = angle - 18
 
-	if angle > 180:
-		angle = 180
-	elif angle < 0:
-		angle = 0
+    if angle > 180:
+        angle = 180
+    elif angle < 0:
+        angle = 0
 
-	setAngle(angle)
-	time.sleep(0.5)
-	return angle
+    setAngle(angle)
+    time.sleep(0.5)
+    return angle
 
 def motorCleanup():
-	'''
-	cleans up GPIO pin and stops servo motor
-	'''
-	pwm.stop()
-	GPIO.cleanup()
+    '''
+    cleans up GPIO pin and stops servo motor
+    '''
+    pwm.stop()
+    GPIO.cleanup()
 
 def motorFullSpin():
-	'''
-	moves the servo motor back and fourth (180 degrees).  Used for testing purposes.
-	'''
+    '''
+    moves the servo motor back and fourth (180 degrees).  Used for testing purposes.
+    '''
 
-	print("Starting to spin\n")
-	try:
-		while True:
-			pwm.ChangeDutyCycle(0)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(2.5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(7.5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(10)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(12.5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(10)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(7.5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(2.5)
-			time.sleep(0.5)
-			pwm.ChangeDutyCycle(0)
-			time.sleep(0.5)
-	except KeyboardInterrupt:
-		motorCleanup()
+    print("Starting to spin\n")
+    try:
+        while True:
+            pwm.ChangeDutyCycle(0)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(12.5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(0)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        motorCleanup()
 
 def controlMotorAngle():
-	'''
-	Allows user to input desired angle for servo motor to point to.  Used for testing purposes
-	'''
-	try:
-		while True:
-			angle = input("Enter angle: ")
-			print angle
-			setAngle(angle)
-			time.sleep(0.5)
-	except KeyboardInterrupt:
-		motorCleanup()
+    '''
+    Allows user to input desired angle for servo motor to point to.  Used for testing purposes
+    '''
+    try:
+        while True:
+            angle = input("Enter angle: ")
+            print angle
+            setAngle(angle)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        motorCleanup()
